baac928106ccd110d1e544886dbde528f83070b8
Snehil Jain <snehiljain7@gmail.com>
2022-05-09 14:03:12 +0530

    DI Constructor With Annotations

--- src/com/learning/springdemo/TennisCoach.java
+++ src/com/learning/springdemo/TennisCoach.java
@@ -1,14 +1,31 @@
 package com.learning.springdemo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class TennisCoach implements Coach {
 
+	private FortuneService fortuneService;
+	
+	@Autowired
+	public TennisCoach(FortuneService fortuneService) {
+		super();
+		this.fortuneService = fortuneService;
+	}
+	
 	@Override
 	public String getDailyWorkout() {
 		// TODO Auto-generated method stub
 		return "Practice your backhand volley";
+	}
+
+	
+
+	@Override
+	public String getDailyFortune() {
+		// TODO Auto-generated method stub
+		return fortuneService.getFortune();
 	}
 
 }
