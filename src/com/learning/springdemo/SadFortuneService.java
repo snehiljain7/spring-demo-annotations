007e03a4c20e0f31ae0967aac1e56d5822dc9efe
Snehil Jain <snehiljain7@gmail.com>
2022-05-12 17:26:46 +0530

    Spring Config With Java Code

---
+++ src/com/learning/springdemo/SadFortuneService.java
@@ -0,0 +1,11 @@
+package com.learning.springdemo;
+
+public class SadFortuneService implements FortuneService {
+
+	@Override
+	public String getFortune() {
+		// TODO Auto-generated method stub
+		return "Today is a sad day";
+	}
+
+}
